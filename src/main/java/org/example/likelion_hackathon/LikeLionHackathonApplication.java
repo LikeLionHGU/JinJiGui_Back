11cf276d56ac66e66dd374ca8c52da41df8fcb03
ìºzionkim <22100130@handong.ac.kr>
2025-02-02 20:36:42 +0900

    feat : create project

---
+++ src/main/java/org/example/likelion_hackathon/LikeLionHackathonApplication.java
@@ -0,0 +1,13 @@
+package org.example.likelion_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLionHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/likelion_hackathon/LikeLionHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.likelion_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
