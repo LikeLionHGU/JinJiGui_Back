fed7fabff9a8e362d400be17318c21891f61c73e
ìºzionkim <22100130@handong.ac.kr>
2025-02-17 03:13:04 +0900

    feat : modify >>> HolderController -> 21row -> add ResponseEntity

--- src/main/java/org/example/likelion_hackathon/controller/HolderController.java
+++ src/main/java/org/example/likelion_hackathon/controller/HolderController.java
@@ -3,7 +3,9 @@ package org.example.likelion_hackathon.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.likelion_hackathon.controller.response.holder.HolderResponse;
 import org.example.likelion_hackathon.service.HolderService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,7 @@ public class HolderController {
     private final HolderService holderService;
 
     @GetMapping("/manager/holder/{scheduleId}")
-    public List<HolderResponse> getHolders(@PathVariable("scheduleId") Long scheduleId) {
-        return holderService.makeHolderResponse(scheduleId);
+    public ResponseEntity<List<HolderResponse>> getHolders(@PathVariable("scheduleId") Long scheduleId) {
+        return ResponseEntity.ok().body(holderService.makeHolderResponse(scheduleId));
     }
 }
